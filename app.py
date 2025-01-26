bead9d38a20855e733adae2d1ce2868f9c6b6256
hamid mujtaba <hamipirzada@gmail.com>
2025-01-27 02:09:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ async def fetch_documents(url):
                 st.warning("Could not retrieve an English transcript. Trying Hindi...")
                 loader = YoutubeLoader.from_youtube_url(url, add_video_info=True, language="hi")
         else:
-            loader = UnstructuredURLLoader(urls=[url], ssl_verify=True, headers={
+            loader = UnstructuredURLLoader(urls=[url], ssl_verify=False, headers={
                 "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 13_5_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"
             })
         documents = loader.load()
